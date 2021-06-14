008f157f1eebfb6c822d7c6deee8ca6f336d068f
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-06-14 13:26:25 +0530

    basic Structure Complete

---
+++ src/Components/User.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './user.css'
+function User() {
+    return (<>
+        <div className="container"> 
+            <h1>USer page</h1>
+            
+
+        </div>
+        </>
+    )
+}
+
+export default User
