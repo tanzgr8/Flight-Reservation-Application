2cd813f34797cc8868a17f8e83ad7a4f5d08439b
Tanzeel Ahmed <tanzeelgr8@gmail.com>
2021-06-15 13:36:28 +0530

    Make Beautiful

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import './App.css'
 function App() {
   return (
    <>
+   <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Air Asia</h1>
+    <p class="lead">Book your Tickets Here</p>
+  </div>
+</div>
     <div className="container ct" > 
       <div className="row">
         <div className="col-sm-6">
-    <Link className="btn btn-success btn-lg" role="button" to="/adminLogin">
-            Admin Login
+    <Link className="btn btn-success btn-lg zz" role="button" to="/adminLogin">
+    <i className="arrow left icon "></i> Admin Login
             </Link>
             </div>
             <div className="col-sm-6">
-    <Link className="btn btn-success btn-lg" role="button" to="/UserLogin">
+    <Link className="btn btn-success btn-lg zz" role="button" to="/UserLogin">
         User Login <i className="arrow right icon "></i>
       </Link>
       </div>
